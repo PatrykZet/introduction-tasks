ff320bcec035d44af4dd49e1e078ff9953ca3299
PatrykZet <59501472+PatrykZet@users.noreply.github.com>
2021-12-06 21:22:14 +0100

    Update Task1_1.java
    Here my solution :)

--- src/main/java/pl/sda/introductiontasks/tasks1/Task1_1.java
+++ src/main/java/pl/sda/introductiontasks/tasks1/Task1_1.java
@@ -5,9 +5,9 @@ public class Task1_1 {
   Create a variable named 'x' and initialize it with any integer.
   In case 'x' is even, in the console print 'Is even', else print 'Is odd'.
    */
-  public static void main(String[] args) {
-    /*
-    Write your code here
-   */
-  }
+        public static void main(String[] args) {
+     int x = 5;
+            System.out.println("x jest nie parzyste");
+        }
+    }
 }
